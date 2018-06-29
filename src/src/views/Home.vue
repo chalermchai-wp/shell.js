6f974b63bf2e023d8d41fad7ffabf1dbfc5abced
Davide Caruso <davide.caruso93@gmail.com>
2018-06-29 19:20:53 +0200

    🚧 Going ahead with homepage
    Update dependencies; update homepage elements and layout

--- src/src/views/Home.vue
+++ src/src/views/Home.vue
@@ -4,15 +4,22 @@
       <div class="hero-body">
         <div class="container has-text-centered">
           <h1 class="title">Shell.js</h1>
-          <h2 class="subtitle">A <strong>JavaScript</strong> library to create HTML <strong>terminals</strong> in web pages. </h2>
+          <h2 class="subtitle is-3">A <strong>JavaScript</strong> library to create HTML <strong>terminals</strong> in web pages. </h2>
+          <div id="shell"></div>
+
           <pre class="home-hero">
 <code><span class="is-unselectable">$ </span>yarn add shell.js</code>
-        </pre>
+          </pre>
+
+          <div class="buttons">
+            <a class="github-button" href="https://github.com/davidecaruso/shell.js" data-icon="octicon-star"
+               data-size="large" data-show-count="true" aria-label="Star davidecaruso/shell.js on GitHub">Star</a>
+            <a class="github-button" href="https://github.com/davidecaruso/shell.js/fork"
+               data-icon="octicon-repo-forked" data-size="large" data-show-count="true"
+               aria-label="Fork davidecaruso/shell.js on GitHub">Fork</a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="hero">
-      <div id="shell"></div>
     </div>
   </section>
 </template>
@@ -26,22 +33,28 @@
     data () {
       return {}
     },
+    created () {
+      let script = document.createElement('script')
+      script.setAttribute('src', 'https://buttons.github.io/buttons.js')
+      document.head.appendChild(script)
+    },
     mounted () {
       return new Shell('#shell', {
         user: 'guest',
-        host: 'shell.js',
+        host: 'davidecaruso.github.io',
         path: '~',
         style: 'ubuntu',
         theme: 'dark',
         responsive: false,
         typed: Typed,
         commands: [
+          '# I\'ll write as root',
           'sudo -i',
-          'Hello bro',
-          'This is an example of what you can do with Shell.js',
-          'Look at the examples page to see other styles',
-          'Or check out the doc for details',
-          'Stay tuned'
+          '# hello bro',
+          '# this is what you can do with shell.js',
+          '# look at the examples page to see other styles and themes',
+          '# and check out the documentation for details',
+          'exit'
         ]
       })
     }
@@ -52,9 +65,22 @@
   @import "../style/config";
 
   #shell {
-    margin: auto;
+    margin: 0 auto 1rem;
     width: 100%;
     height: 40vh;
+  }
+
+  h1.title {
+    color: transparent;
+    background-color: white;
+    -webkit-mask: url(/static/logo.svg) no-repeat 50% 50%;
+    mask: url(/static/logo.svg) no-repeat 50% 50%;
+    font-size: 7rem;
+  }
+
+  .buttons {
+    justify-content: center;
+    margin: 1rem 0;
   }
 
   pre {
